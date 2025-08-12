2a53932ba9c5177e046b4905fd085008ea30fc89
Username387J <jdeleijagbulu@gmail.com>
2025-08-11 19:44:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ screen_height = 700
 screen = pygame.display.set_mode([screen_width, screen_height])
 
 def changebg(img):
-    bg = pygame.image.load(img)
-    bg = pygame.transform.scale(bg,(screen_width,screen_height))
+    background = pygame.image.load(img)
+    bg = pygame.transform.scale(background,(screen_width,screen_height))
     screen.blit(bg,(0,0))
 
 class Bin(pygame.sprite.Sprite):
@@ -24,7 +24,7 @@ class Recyclable(pygame.sprite.Sprite):
     def __init__(self,img):
         super().__init__()
         self.image=pygame.image.load(img)
-        self.image=pygame.transform.scale(img,(30,30))
+        self.image=pygame.transform.scale(self.image,(30,30))
         self.rect=self.image.get_rect()
 
 class NonRecyclable(pygame.sprite.Sprite):
@@ -47,8 +47,8 @@ images = ["item1.png", "item2.png", 'item3.png']
 
 for i in range (50):
     item=Recyclable(random.choice(images))
-    item.rect.x=random.randrage(screen_width)
-    item.rect.y=random.randrage(screen_height)
+    item.rect.x=random.randrange(screen_width)
+    item.rect.y=random.randrange(screen_height)
 
     item_list.add(item)
     allsprites.add(item)
@@ -70,3 +70,61 @@ play = True
 score = 0
 clock = pygame.time.Clock()
 start_time = time.time()
+
+myFont = pygame.font.SysFont("Times New Roman", 22)
+text = myFont.render("Score =" + str(0), True, BLACK)
+
+while play:
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            play = False
+    
+    timeElapsed=time.time()-start_time
+
+    if timeElapsed >= 20:
+        if score >= 20:
+            screen.fill(GREEN)
+            text1=myFont.render("Bin loot successful",True,RED)
+        else:
+            screen.fill(RED)
+            text1=myFont.render("Try again later",True,WHITE)
+
+        screen.blit(text1,(250,40))
+    else:
+        changebg("bground.png")
+        countDown = myFont.render("Time Left:" + str(60 - int(timeElapsed)), True, WHITE)
+        screen.blit(countDown, (20,10))
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            if bin.rect.y >0:
+                bin.rect.y -= 5
+        if keys[pygame.K_DOWN]:
+            if bin.rect.y <630:
+                bin.rect.y += 5
+        if keys[pygame.K_LEFT]:
+            if bin.rect.x>0:
+                bin.rect.x -=5
+        if keys[pygame.K_RIGHT]:
+            if bin.rect.x<850:
+                bin.rect.x +=5
+
+        item_hit_list=pygame.sprite.spritecollide(bin, item_list,True)
+
+        for item in item_hit_list:
+            score +=1
+            text = myFont.render("Score =" + str(score), True,WHITE)
+
+        plastic_hit_list=pygame.sprite.spritecollide(bin,plastic_list,True)
+
+        for plastic in plastic_hit_list:
+            score -= 5
+            text = myFont.render("Score =" + str(score), True,WHITE)
+
+    screen.blit(text,(20,50))
+    allsprites.draw(screen)
+    pygame.display.update()
+pygame.quit()
